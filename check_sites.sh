f453d50c7a51dcf1c4f33bf0886100bf30712acc
RTS007 <RTS007@users.noreply.github.com>
2022-03-01 23:01:13 +0100

    Update check_sites.sh
    add quotes

--- check_sites.sh
+++ check_sites.sh
@@ -7,7 +7,13 @@ STD='\033[0m'    # Reset
 OK='\033[0;32m'  # Green
 ERR='\033[0;31m' # Red
 
-SITES=(www.gosuslugi.ru www.mos.ru www.kremlin.ru www.government.ru www.mil.ru www.nalog.gov.ru www.customs.gov.ru www.pfr.gov.ru www.rkn.gov.ru www.sberbank.ru online.sberbank.ru www.vtb.ru www.gazprombank.ru www.gazprom.ru www.lukoil.ru www.magnit.ru www.nornickel.com www.surgutneftegas.ru www.tatneft.ru www.evraz.com www.nlmk.com www.sibur.ru www.severstal.com www.metalloinvest.com www.nangs.org www.rmk-group.ru www.tmk-group.ru www.polymetalinternational.com www.uralkali.com www.eurosib.ru www.omk.ru)
+SITES=("www.gosuslugi.ru" "www.mos.ru" "www.kremlin.ru" "www.government.ru"
+   "www.mil.ru" "www.nalog.gov.ru" "www.customs.gov.ru" "www.pfr.gov.ru" "www.rkn.gov.ru"
+   "www.sberbank.ru" "online.sberbank.ru" "www.vtb.ru" "www.gazprombank.ru" "www.gazprom.ru"
+   "www.lukoil.ru" "www.magnit.ru" "www.nornickel.com" "www.surgutneftegas.ru"
+   "www.tatneft.ru" "www.evraz.com" "www.nlmk.com" "www.sibur.ru" "www.severstal.com"
+   "www.metalloinvest.com" "www.nangs.org" "www.rmk-group.ru" "www.tmk-group.ru"
+   "www.polymetalinternational.com" "www.uralkali.com" "www.eurosib.ru" "www.omk.ru")
 
 while [ 1 ]; do
    for l in "${SITES[@]}"; do
